5cf08ab005b670d34292353104eba9572acd9c8f
Egor Prilepin <Egorprilepin548@icloud.com>
2021-11-09 13:11:14 +0300

    added development plan

--- main.cpp
+++ main.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
+void numbers_of_products_div()
+{
+    /**
+    1. Считать и запомнить входные данные
+    2. Вывести все пары чисел
+    3. Вместо вывода пар посчитать произведение
+    4. Вывести пары, произведение кторых равны 22
+    5. Только коллличество пар
+    */
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    numbers_of_products_div();
     return 0;
 }
