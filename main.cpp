5e1afbb59209c3ddd5b8ec1dc9bc313dc22bc595
Egor Prilepin <Egorprilepin548@icloud.com>
2021-11-16 13:33:38 +0300

    added 4th line

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ void max_sum_div()
     ifstream ifs("data.txt");
     /**
     + 1. Считать и сохранить все числа
-    2. Вывести все пары
-    3. Вместо пары вывести суммы
+    + 2. Вывести все пары
+    + 3. Вместо пары вывести суммы
     4. Проверить делимость суммы
     5. Вывести только ответ
     */
@@ -25,6 +25,7 @@ void max_sum_div()
     cout<<endl;
     for (int j(0); j<n-1; j++)
         for (int i(j+1); i<n; i++)
+            if ((numbers[i] + numbers[j])%2 != 0)
             cout << numbers[j] + numbers[i] << endl;
 
 }
