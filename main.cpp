b649228c3585c223400f0e4ebff36bebc17eba7d
Egor Prilepin <Egorprilepin548@icloud.com>
2021-11-30 12:23:58 +0300

    effective code done

--- main.cpp
+++ main.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
+
 using namespace std;
-void number_of_products_div()
-{
-    /*
-	1 считываем входные данные и выводим их
-	2 находим делители 30
-	3 если входное число делится на найденный делитель, то прибавляем +1
-	4 выводим количество пар
-	*/
-	long long int n(0), cur(0);
-	long long int n30(0), n2(0), n3(0), n5(0), n6(0), n10(0), n15(0), no(0);
-    cin >> n;
-    for (int i(0); i<n; i++)
-    {
-        cin >> cur;
-        if (cur % 30 == 0)
-			n30++;
-		else if (cur%2==0)
-			n2++;
-		else if (cur%3==0)
-			n3++;
-        else if (cur%5==0)
-			n5++;
-        else if (cur%2==0 and cur%3==0)
-			n5++;
-    }
-    no = n - n30 - n2 - n3 - n5;
-    cout << (n30*(n30-1)/2)  +  n30*no  +  n30*n2  +  n30*n3  +  n30*n5  +
+
+void number_of_products_div() {
+
+long long int n(0), cur(0);
+long long int n165(0), n55(0), n33(0), n15(0), n11(0), n5(0), n3(0), n0(0);
+cin >> n;
+for (int i(0); i < n; ++i) {
+    cin >> cur;
+    if (cur % 165 == 0)
+        n165++;
+    else if (cur % 55 == 0)
+        n55++;
+    else if (cur % 33 == 0)
+        n33++;
+    else if (cur % 15 == 0)
+        n15++;
+    else if (cur % 11 == 0)
+        n11++;
+    else if (cur % 5 == 0)
+        n5++;
+    else if (cur % 3 == 0)
+        n3++;
+    else
+        n0++;
+}
+cout << n165 * (n165 - 1) / 2 + n165 * (n55 + n33 + n15 + n11 + n5 + n3) + (n55 * n33 + n55 * n15 + n33 * n15) + (n55 * n3 + n33 * n5 + n15 * n11) + n0 * n165;
 }
 
 int main()
 {
-    number_of_products_div();
-    return 0;
+number_of_products_div();
+return 0;
 }
