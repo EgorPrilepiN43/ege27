249ecbff64bb3f10cb9b06e8c4caa3bf7757ead2
Egor Prilepin <Egorprilepin548@icloud.com>
2021-11-30 12:52:44 +0300

    added first and second steps of the plan

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+void number_of_products_div()
+{
+    /*
+    1.+Считать числа
+    2.+Вывести все числа
+    3.вывести все пары
+    4.Вывести тольео достаточно далекие пары
+    5.Вместо вывода пар - выводим сумму
+    6.Минимальная сумма - ответ
+	*/
+    int n(0);
+    cin >> n;
+    vector <int> numbers(n);
+    for (int i(0); i<n; i++)
+        cin >> numbers[i];
+    cout << endl;
+    for (int i(0); i<n; i++)
+        cout << numbers[i] <<" ";
+
+}
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    number_of_products_div();
     return 0;
 }
