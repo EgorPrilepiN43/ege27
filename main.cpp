0db29a54efa11efb5f94f30c71f77e11b6fc5707
Egor Prilepin <Egorprilepin548@icloud.com>
2021-12-23 15:27:31 +0300

    choose only needs pairs

--- main.cpp
+++ main.cpp
@@ -8,9 +8,8 @@ void num_sum_itog_ineff()
 
     /*
     ++1. Вводим и выводим числа правильно, согласно условию
-    2.
+    ++2. Циклами перебераем тройки, которые подходят под условие
     3.
-    4.
 
     */
     int n(0);
@@ -20,10 +19,21 @@ void num_sum_itog_ineff()
     int cnt(0);
     int res(1001);
     for (int i(0); i<n; i++)
-        cin >> numbers[i];
-    cout << endl;
-    for (int i(0); i<n; i++)
-        cout << numbers[i] <<" ";
+    {
+        cin>>numbers[i];
+    }
+    for (int j(0); j<n-2; j++)
+        for (int i(j+1); i<n-1;i++)
+            for (int a(i+1); a<n;a++)
+            {
+                if ((numbers[i]+numbers[j]+numbers[a])%4==0 and (numbers[i]+numbers[j]+numbers[a])<res)
+                {
+                    cout<<numbers[j]<<" "<<numbers[i]<<" "<<numbers[a]<<" "<< endl;
+                    res=numbers[i]+numbers[j]+numbers[a];
+                    cnt++;
+
+
+                }
 }
 int main()
 {
